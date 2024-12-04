4e17689c01f95c4de0d090f98e9481e69e6d1bfb
JacobStewart-Coding <jakestewart@hotmail.ca>
2024-12-04 17:54:50 -0500

    Added savetransaction method and updated borrow/return methods.

--- Library/Transaction.java
+++ Library/Transaction.java
@@ -1,6 +1,3 @@
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public class Transaction {
 
 	
@@ -24,6 +21,7 @@ public class Transaction {
             member.borrowBook(book); 
             String transactionDetails = getCurrentDateTime() + " - Borrowing: " + member.getName() + " borrowed " + book.getTitle();
             System.out.println(transactionDetails);
+            saveTransaction(transactionDetails);
             return true;
         } else {
             System.out.println("The book is not available.");
@@ -39,6 +37,7 @@ public class Transaction {
             book.returnBook();
             String transactionDetails = getCurrentDateTime() + " - Returning: " + member.getName() + " returned " + book.getTitle();
             System.out.println(transactionDetails);
+            saveTransaction(transactionDetails);
         } else {
             System.out.println("This book was not borrowed by the member.");
         }
@@ -48,5 +47,15 @@ public class Transaction {
     private static String getCurrentDateTime() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return sdf.format(new Date());
+    }
+    
+    public void saveTransaction(String transactionDetails) {
+        try {
+            FileWriter writer = new FileWriter("transactions.txt", true);
+            writer.write(transactionDetails + System.lineSeparator());
+            writer.close();
+        } catch (Exception e) {
+            System.out.println("An error occurred while writing to the file.");
+        }
     }
 }
